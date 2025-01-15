40445c0ccf7e93746ff1b3f0b2b4e95a9ba35ec7
YCY <chengyanyang0510@gmail.com>
2025-01-15 22:58:00 +0800

    update example to use the endchat function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Changelog
 
 All notable changes to this project will be documented in this file.
+## [0.4.0+1]
+- Update example
 
 ## [0.4.0]
 - Add method to get chatOngoing visitorEngaged and endChat
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,15 +9,55 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TawkController? _controller;
+  final _messangerKey = GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+      key: _messangerKey,
+      child: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter Tawk.to'),
           elevation: 0,
+          actions: [
+            OutlinedButton(
+              onPressed: _controller == null
+                  ? null
+                  : () async {
+                      if (await _controller!.isChatOngoing()) {
+                        bool? confirm = await showAlert();
+                        if (confirm == true) {
+                          _controller!.endChat();
+                        }
+                      } else {
+                        _messangerKey.currentState!.showSnackBar(const SnackBar(
+                          content: Text('No ongoing chat'),
+                          behavior: SnackBarBehavior.floating,
+                        ));
+                      }
+                    },
+              child: const Text('End Chat'),
+            )
+          ],
         ),
         body: Tawk(
           directChatLink:
-              'https://tawk.to/chat/615847b0d326717cb68462c7/', //'YOUR_DIRECT_CHAT_LINK',
+              'https://tawk.to/chat/615847b0d326717cb68462c7', //'YOUR_DIRECT_CHAT_LINK',
           visitor: TawkVisitor(
             name: 'Username',
             email: 'example@gmail.com',
@@ -31,8 +71,38 @@ class MyApp extends StatelessWidget {
           placeholder: const Center(
             child: Text('Loading...'),
           ),
+          onControllerChanged: (value) {
+            setState(() {
+              _controller = value;
+            });
+          },
         ),
       ),
     );
+  }
+
+  Future<bool?> showAlert() async {
+    return await showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('End Chat'),
+            content: const Text('Are you sure you want to end the chat?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('End Chat'),
+              ),
+            ],
+          );
+        });
   }
 }
